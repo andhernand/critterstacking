f94ddd29fa8a113e03612bc06adb623992d20024
Andres Hernandez <andhernand@gmail.com>
2024-09-14 13:35:01 -0600

    add wolverine and marten dependencies

--- test/Wolvie.Tests/SanityCheck.cs
+++ test/Wolvie.Tests/SanityCheck.cs
@@ -2,23 +2,19 @@
 
 using FluentAssertions;
 
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace Wolvie.Tests;
 
-public class SanityCheck(WolvieFactory wolvieFactory) : IClassFixture<WolvieFactory>, IDisposable
+public class SanityCheck(WolvieFactory wolvieFactory) : IClassFixture<WolvieFactory>
 {
-    private readonly HttpClient _client = wolvieFactory.CreateClient();
+    private readonly HttpClient _client = wolvieFactory.CreateClient(
+        new WebApplicationFactoryClientOptions { BaseAddress = new Uri("https://localhost") });
 
     [Fact]
     public async Task Swagger_WhenCalled_ReturnsOk()
     {
         var response = await _client.GetAsync("/swagger");
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-    }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-        _client.Dispose();
-        wolvieFactory.Dispose();
     }
 }
